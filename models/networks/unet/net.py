fc02c5ab860a8c3e7d0a1fdeaf9c41e6b3a84253
chaeyeongyun <ui88g216@gmail.com>
2023-04-11 13:12:23 +0900

    .

--- models/networks/unet/net.py
+++ models/networks/unet/net.py
@@ -137,3 +137,47 @@ class VQUnet_v2(nn.Module):
         
         for param in self.codebook.parameters():
             param.requires_grad = False
+
+class Unet(nn.Module):
+    def __init__(
+        self, 
+        encoder_name:str,
+        num_classes:int,
+        in_channels:int=3,
+        decoder_channels=None,
+        depth:int=5,
+        activation=nn.Identity,
+        upsampling=2,
+        ):
+        super().__init__()
+        
+        self.encoder = make_encoder(encoder_name, in_channels, depth)    
+        encoder_channels = self.encoder.out_channels()
+        
+        if decoder_channels == None:
+            decoder_channels = [i//2 for i in encoder_channels[1:]] 
+            decoder_channels = decoder_channels[::-1]
+        self.decoder = UnetDecoder(encoder_channels,
+                                   decoder_channels)
+        self.segmentation_head = SegmentationHead(in_channels=decoder_channels[-1],
+                                                  out_channels=num_classes,
+                                                  upsampling=upsampling,
+                                                  activation=activation,
+                                                  kernel_size=3)
+    def forward(self, x):
+        features = self.encoder(x)[1:]
+        decoder_out = self.decoder(*features)
+        output = self.segmentation_head(decoder_out)
+        
+        return output
+
+    def load_pretrained(self, encoder_weights_path, codebook_weights_path):
+        print(f"... load pretrained weights ... \nencoder:{encoder_weights_path}, codebook:{codebook_weights_path}")
+        encoder_weights = torch.load(encoder_weights_path)
+        codebook_weights = torch.load(codebook_weights_path)
+        self.encoder.load_state_dict(encoder_weights)
+        self.codebook.load_state_dict(codebook_weights)
+    
+    def freeze_encoder(self):
+        for param in self.encoder.parameters():
+            param.requires_grad = False
--- unzip.py
+++ unzip.py
@@ -14,7 +14,7 @@ def unzip_all(path_head, extract_to):
 
 if __name__ == '__main__':
     root = '/content/downloads'
-    name_list = ["VQUnet_v16-20230330T030506Z", "VQUnet_v23-20230329T032936Z", "VQUnet_v24-20230330T030506Z"]
+    name_list = ["ckpoints-20230411T021348Z"]
     for name in name_list:
         path = os.path.join(root, name)
         print(path)
