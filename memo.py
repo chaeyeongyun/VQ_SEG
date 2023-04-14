5b718bafabfc0ea47549ad3f1c67a4a92c9cf109
chaeyeongyun <ui88g216@gmail.com>
2023-04-14 18:45:04 +0900

    .

--- memo.py
+++ memo.py
@@ -1,6 +1,11 @@
+from tqdm import tqdm
+import os
+from glob import glob
 import torch
 from models.networks import VQUnet_v1, VQVAEv2
 import tarfile
+from saliency_map import get_saliency_rbd
+import matplotlib.pyplot as plt
 if __name__ == '__main__':
     # model = VQVAEv2('resnet50', {
     #             "num_embeddings":2048,
@@ -9,5 +14,13 @@ if __name__ == '__main__':
     # x = torch.randn(4, 3, 512, 512)
     # output = model(x)
     # a=1
-    with tarfile.open("mytext.tar.gz",'w:gz') as mytar:
-        mytar.add('./ttt')
+    # with tarfile.open("mytext.tar.gz",'w:gz') as mytar:
+    #     mytar.add('./ttt')
+    folders  = ["/content/data/semi_sup_data/CWFID/num30/train/input", "/content/data/semi_sup_data/CWFID/num30/test/input"]
+    save = "/content/data/semi_sup_data/CWFID/salient_map"
+    for folder in folders:
+        imgs = glob(os.path.join(folder, "*.png"))
+        for img in tqdm(imgs):
+            rbd = get_saliency_rbd(img).astype('uint8')
+            filename = os.path.split(img)[-1]
+            plt.imsave(os.path.join(save, filename), rbd, cmap='gray')
