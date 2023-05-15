ea0526eab5116f95878fb374a87997d0c9b4f9ac
chaeyeongyun <ui88g216@gmail.com>
2023-05-15 17:14:11 +0900

    .

--- utils/device.py
+++ utils/device.py
@@ -4,5 +4,7 @@ from typing import Union
 def device_setting(device:Union[int, str]):
     if device in ['-1', -1, 'cpu']:
         return torch.device('cpu')
+    elif device == 'cuda':
+        return torch.device('cuda')
     else:
         return torch.device('cuda:'+str(device))
