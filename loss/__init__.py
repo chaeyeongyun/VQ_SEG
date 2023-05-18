9c049a474847b0d5695a3e0ba19ca5b8aa29274a
chaeyeongyun <ui88g216@gmail.com>
2023-05-18 17:36:38 +0900

    .

--- loss/__init__.py
+++ loss/__init__.py
@@ -18,7 +18,7 @@ loss_func_dict = {'cross_entropy':F.cross_entropy,
                  'nll_loss':F.nll_loss}
 
 def make_loss(loss_name:str, num_classes:int, ignore_index:int=-100, weight:torch.Tensor=None):    
-    if loss_name == 'cross_entropy':
+    if loss_name in ['cross_entropy', 'nll_loss']:
         return loss_dict[loss_name](ignore_index=ignore_index, weight=weight)
     else:
         return loss_dict[loss_name](num_classes=num_classes, ignore_index=ignore_index, weight=weight)
