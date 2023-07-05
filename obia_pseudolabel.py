c761c4164f1e9d9e95b51e4e7a84ddcf100a806b
chaeyeongyun <ui88g216@gmail.com>
2023-07-05 11:49:36 +0900

    .

--- obia_pseudolabel.py
+++ obia_pseudolabel.py
@@ -1,3 +1,4 @@
+import os
 import os.path as osp
 import numpy as np
 from skimage.feature import local_binary_pattern
@@ -6,6 +7,7 @@ from fast_slic import Slic
 from PIL import Image
 from glob import glob
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 def img_to_label(target_img, pixel_to_label_dict:dict):
     pixels = list(pixel_to_label_dict.keys())
@@ -35,22 +37,28 @@ def make_feature(image, image_gray, gt_image):
         train_y.append(np.bincount(gt_image[y, x], minlength=3).argmax())
     train_y = np.array(train_y)
     featarray = np.stack(l)
-    return featarray, train_y
+    return featarray, train_y, assignment
 
-def main(image_root="/content/data/semi_sup_data/CWFID/num30/train"):
-    target_list = glob(osp.join(image_root, "target", "*.png"))
+def main(image_root="/content/data/semi_sup_data/CWFID/num30/train",save_path = "/content/data/semi_sup_data/CWFID/OBIA_pseudo_label"):
+    target_filenames = os.listdir(osp.join(image_root, "target"))
+    
     feat_list = []
     y_list = []
-    for target in tqdm(target_list):
-        filename = osp.split(target)[-1]
-        gt = target
+    # i=0
+    for filename in tqdm(target_filenames):
+        ## debug
+        # i += 1
+        # if i==5:
+        #     break
+        ##
+        gt = osp.join(image_root, "target", filename)
         img = osp.join(image_root, "input", filename)
         image =Image.open(img).convert("RGB")
         image_gray = np.array(image.convert("L"))
         image = np.array(image)
         gt_image  = np.array(Image.open(gt).convert("L"))
         gt_image = img_to_label(gt_image, {0:0, 128:1, 255:2})
-        featarray, train_y =  make_feature(image, image_gray, gt_image)
+        featarray, train_y, _ =  make_feature(image, image_gray, gt_image)
         feat_list.append(featarray)
         y_list.append(train_y)
 
@@ -59,8 +67,25 @@ def main(image_root="/content/data/semi_sup_data/CWFID/num30/train"):
     model = RandomForestClassifier()
     model.fit(features, Y_train)
     ## TODO: unlabeled data labeling
-    
-    a=1
+    image_filenames = os.listdir(osp.join(image_root, "input"))
+    unlabeled_filenames = list(set(image_filenames) - set(target_filenames))
+    # i = 0
+    for filename in tqdm(unlabeled_filenames):
+        ## debug
+        # i += 1
+        # if i==5:
+        #     break
+        ##
+        img = osp.join(image_root, "input", filename)
+        image =Image.open(img).convert("RGB")
+        image_gray = np.array(image.convert("L"))
+        image = np.array(image)
+        featarray, train_y, assignment =  make_feature(image, image_gray, gt_image)
+        pred = model.predict(featarray)
+        pseudo_label = pred[assignment]
+        pseudo_label = img_to_label(pseudo_label, {0:0, 1:128, 2:255})
+        plt.imsave(osp.join(save_path, filename), pseudo_label, cmap="gray")
+       
 
 if __name__ == "__main__":
     main()
