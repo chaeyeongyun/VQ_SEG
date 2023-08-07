7824bcdb568c0c8abbf61dbd97e482e7f66fbbf9
chaeyeongyun <ui88g216@gmail.com>
2023-08-07 16:21:01 +0900

    .

--- sample.py
+++
@@ -1,9 +0,0 @@
-from models.encoders.resnet import CCAResNetEncoder
-from torchvision.models.resnet import ResNet, BasicBlock, Bottleneck
-params = {
-            "out_channels": (3, 64, 256, 512, 1024, 2048),
-            "block": Bottleneck,
-            "layers": [3, 4, 6, 3],
-        }
-m = CCAResNetEncoder(**params)
-print(m)
