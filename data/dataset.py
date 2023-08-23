b5cd2fc8f2cad943a8af429ac026bb3b42f96562
chaeyeongyun <ui88g216@gmail.com>
2023-08-23 15:13:53 +0900

    normalized dataset

--- data/dataset.py
+++ data/dataset.py
@@ -7,7 +7,9 @@ from glob import glob
 import torch
 from torch.utils.data import Dataset
 import torchvision.transforms.functional as TF
+from torchvision import transforms
 import torch.nn.functional as F
+
 
 
 class BaseDataset(Dataset):
@@ -158,6 +160,56 @@ class OBIADataset(Dataset):
                 target = target.resize(self.resize, resample=Image.NEAREST)
         
         img = TF.to_tensor(img)
+        target = torch.from_numpy(np.array(target)) if target is not None else None
+        if target is None:
+            return {'filename':filename, 'img':img}
+        return {'filename':filename, 'img':img, 'target':target}
+    
+class NormalizedDataset(Dataset):
+    def __init__(self, data_dir:str, split:str, batch_size:int=None, resize:int=None, target_resize:bool=True, mean=0.5, std=0.5):
+        super().__init__()
+        if type(resize)==int:
+            self.resize = (resize, resize)
+        elif type(resize) in [tuple, list]:
+            self.resize = resize
+        elif resize==None:
+            self.resize = None
+        else:
+            raise ValueError(f"It's invalid type of resize {type(resize)}")
+        
+        self.normalize = transforms.Normalize(mean=mean, std=std)
+        self.img_dir = os.path.join(data_dir, 'input')
+        self.target_resize = target_resize
+        if split == 'labelled':
+            self.filenames = os.listdir(os.path.join(data_dir, 'target'))
+            self.target_dir = os.path.join(data_dir, 'target')
+        elif split == 'unlabelled':
+            self.filenames = list(set(os.listdir(os.path.join(data_dir, 'input'))) - set(os.listdir(os.path.join(data_dir, 'target'))))
+            self.target_dir = None
+        else:
+            raise ValueError(f"split has to be labelled or unlabelled")
+        
+        if batch_size is not None and len(self.filenames) % batch_size != 0:
+                self.filenames = self.filenames + self.filenames[0:batch_size-len(self.filenames) % batch_size]
+                
+    def __len__(self):
+        return len(self.filenames)
+    
+    def __getitem__(self, index):
+        filename = self.filenames[index]
+        img = Image.open(os.path.join(self.img_dir, filename)).convert('RGB')
+        if self.target_dir is not None:
+            target = Image.open(os.path.join(self.target_dir, filename)).convert('L')
+        else:
+            target = None
+        
+        if self.resize is not None:
+            img = img.resize(self.resize, resample=Image.BILINEAR)
+            if self.target_resize and target is not None:
+                target = target.resize(self.resize, resample=Image.NEAREST)
+        
+        img = TF.to_tensor(img)
+        img = self.normalize(img)
         target = torch.from_numpy(np.array(target)) if target is not None else None
         if target is None:
             return {'filename':filename, 'img':img}
